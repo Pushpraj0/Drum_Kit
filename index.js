a152ff16af8ef47994ca089b0cd13502facf447d
Pushpraj0 <97541059+Pushpraj0@users.noreply.github.com>
2023-08-24 18:22:14 +0530

    Adding files of Drum kit website
    adding
    index.html, style.css, index.js, images, sounds
    You can play drum by clicking or pressing key in the images shown in the frontend

---
+++ index.js
@@ -0,0 +1,50 @@
+
+//detecting button press
+var numOfTimeClicked = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numOfTimeClicked; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        // console.log(this);
+        var buttoninnerHTML = this.innerHTML;
+        makesound(buttoninnerHTML)
+    });
+}
+// detecting key press
+document.addEventListener("keypress", function (event) {
+    makesound(event.key);
+});
+
+// function to make sound
+function makesound(key) {
+    switch (key) {
+        case "w":
+            tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            kickbass = new Audio("sounds/kick-bass.mp3");
+            kickbass.play();
+            break;
+        default: (console.log(buttoninnerHTML));
+            break;
+    }
+}
